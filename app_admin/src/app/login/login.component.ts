d00c4b127223fef7217de548f4751866d56d0cf8
dandrews404 <111705037+dandrews404@users.noreply.github.com>
2023-12-12 16:11:47 -0600

    Update login.component.ts

--- app_admin/src/app/login/login.component.ts
+++ app_admin/src/app/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthenticationService } from
-'../services/authentication.service';
+import { AuthenticationService } from '../../app/services/authentication';
 import { User } from '../models/user';
 @Component({
 selector: 'app-login',
