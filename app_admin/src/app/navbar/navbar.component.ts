f052e3f2c2ae80ae7acec65d8768c64cab808591
dandrews404 <111705037+dandrews404@users.noreply.github.com>
2023-12-12 16:07:40 -0600

    update

--- app_admin/src/app/navbar/navbar.component.ts
+++ app_admin/src/app/navbar/navbar.component.ts
@@ -1,24 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthenticationService } from '../services/authentication';
-
+import { AuthenticationService } from '../services/authentication.service';
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+selector: 'app-navbar',
+templateUrl: './navbar.component.html',
+styleUrls: ['./navbar.component.css']
 })
-
 export class NavbarComponent implements OnInit {
-  constructor(
-  private authenticationService: AuthenticationService
+constructor(
+ private authenticationService: AuthenticationService
 ) { }
-
 ngOnInit() { }
-
 public isLoggedIn(): boolean {
-  return this.authenticationService.isLoggedIn();
+ return this.authenticationService.isLoggedIn();
 }
-
 private onLogout(): void {
-  return this.authenticationService.logout();
+ return this.authenticationService.logout();
 }
 }
